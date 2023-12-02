d312c0bd00d56aa427fbed81626bd5266179d8fa
akm <akm2000@gmail.com>
2023-12-02 17:20:02 +0900

    :wrench: make goa_gen と make goa_example を追加

---
+++ apisvr/Makefiles/goa.mk
@@ -0,0 +1,17 @@
+# This file uses the following variables or tasks from
+#
+# Depends on go.mk
+#   - GO_ROOT_PACKAGE
+GOA_ROOT_PACKAGE=$(GO_ROOT_PACKAGE)
+GOA_SERVICES_DIR=./services
+
+GOA_GEN_DIR=./gen
+$(GOA_GEN_DIR): goa_gen
+
+.PHONY: goa_gen
+goa_gen:
+	goa gen $(GOA_ROOT_PACKAGE)/design -o $(GOA_SERVICES_DIR)
+
+.PHONY: goa_example
+goa_example:
+	goa example $(GOA_ROOT_PACKAGE)/design -o $(GOA_SERVICES_DIR)
