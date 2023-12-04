4ae01b00ca139cb2cbcee41a1b06340ae6fdfa3f
akm <akm2000@gmail.com>
2023-12-05 08:56:30 +0900

    :sparkles: src/lib/firebase/auth/message_keys.ts を追加

--- ui/src/lib/firebase/auth/index.ts
+++ ui/src/lib/firebase/auth/index.ts
@@ -3,3 +3,6 @@ import { app } from '../app';
 
 export const auth = getAuth(app);
 connectAuthEmulator(auth, 'http://127.0.0.1:9099');
+
+export type { MessageKey } from './message_keys';
+export { messageKeys, isMessageKey } from './message_keys';
