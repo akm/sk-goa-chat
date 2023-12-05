fb42263ca00a1b14e531e81f2585d214583cd77c
akm <akm2000@gmail.com>
2023-12-05 09:07:08 +0900

    :sparkles: src/app.d.ts の App.Locals に user を追加

--- ui/src/app.d.ts
+++ ui/src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { User } from '$lib/models/user';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: User;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
