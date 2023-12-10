c68501f7d8f2c2cade5dc94edfefb649ff67fcdc
akm <akm2000@gmail.com>
2023-12-10 17:38:35 +0900

    :wrench: make での基本的な変数の見直し

--- config.mk
+++ config.mk
@@ -4,11 +4,11 @@
 # - staging
 # - production
 
-# PROCESS_TYPE        | STAGE                    | apisvr      | ui     | mysql    | firebase authentication | メモ
+# Environment type    | STAGE                    | apisvr      | ui     | mysql    | firebase authentication | メモ
 # --------------------|--------------------------|-------------|--------|----------|-------------------------|--------------
 # server              | staging,production       | 8000,8080   | 4173   | (3306)   | (9099)                  |
 # e2e test            | staging,production       | (8000,8080) | (4173) | (3306)   | (9099)                  | 対象のサーバーにアクセスするので特にサーバーを起動したりはしない
-# server              | local                    | 8000,8080   | 5173   | 3306     | 9099
+# dev                 | local                    | 8000,8080   | 5173   | 3306     | 9099
 # e2e test            | local,github             | 8001,8081   | 4173   | 3307     | 9090
 # unit test / apisvr  | local,github             | -,-         | -      | 3311 + N | 9091 + N
 # unit test / ui      | local,github             | -,-         | -      | -        | -
@@ -17,6 +17,32 @@
 # STAGE=github
 # STAGE=staging
 # STAGE=production
-STAGE?=local
+# STAGE=local
 
-MYSQL_DATABASE_NAME=sk_goa_chat_db
+# ENV_TYPE=server
+# ENV_TYPE=e2e_test
+# ENV_TYPE=unit_test
+# ENV_TYPE?=dev
+
+ifeq ($(GITHUB_ACTIONS),true)
+STAGE?=github
+ENV_TYPE?=unit_test
+else
+STAGE?=local
+ENV_TYPE?=dev
+endif
+
+STAGE_ENV_production_server=prodsrv
+STAGE_ENV_staging_server=stgsrv
+STAGE_ENV_local_dev=localdev
+STAGE_ENV_local_e2e_test=locale2e
+STAGE_ENV_local_unit_test=localunit
+STAGE_ENV_github_e2e_test=githube2e
+STAGE_ENV_github_unit_test=githubunit
+STAGE_ENV?=$(STAGE_ENV_$(STAGE)_$(ENV_TYPE))
+
+.PHONY: stage_env
+stage_env:
+	@echo $(STAGE_ENV)
+
+MYSQL_DATABASE_NAME?=sk_goa_chat_db
