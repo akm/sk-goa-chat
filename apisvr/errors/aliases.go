45ee00952620feeb713d1df0c61a9d8db5e3a2d8
akm <akm2000@gmail.com>
2023-12-10 18:26:48 +0900

    :sparkles: apisvr/errors パッケージを追加
    sqlboiler も使用している github.com/friendsofgo/errors を使うように

---
+++ apisvr/errors/aliases.go
@@ -0,0 +1,15 @@
+package errors
+
+import (
+	orig "github.com/friendsofgo/errors"
+)
+
+var (
+	// As        = orig.As
+	// Cause     = orig.Cause
+	// Is        = orig.Is
+	// Errorf    = orig.Errorf
+	// WithStack = orig.WithStack
+	// Wrap      = orig.Wrap
+	Wrapf = orig.Wrapf
+)
