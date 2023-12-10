c5c1a3d8139c69044a509ccce4d9b78b19fe5826
akm <akm2000@gmail.com>
2023-12-10 09:58:19 +0900

    :rotating_light: channels テーブルを追加

---
+++ dbmigrations/20231210095409_create_channels.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE `channels` (
+    id serial PRIMARY KEY,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    name VARCHAR(255) NOT NULL,
+    visibility ENUM('public', 'private') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- +goose Down
+DROP TABLE channels;
