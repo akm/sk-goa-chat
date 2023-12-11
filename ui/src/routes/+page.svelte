a2efab3cbbfcaea614975424b116c51cb318230c
akm <akm2000@gmail.com>
2023-12-11 09:49:49 +0900

    :sparkles: チャンネル一覧を表示

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Input, Label, Checkbox, Button } from 'flowbite-svelte';
+	import { TrashBinSolid } from 'flowbite-svelte-icons';
+	import type { Channel } from '$lib/models/channel';
+	export let data: { channels: Channel[] };
+</script>
+
+<div class="centered">
+	<h1>Channels</h1>
+
+	<ul class="todos">
+		{#each data.channels as channel (channel.id)}
+			<li>
+				<span>{channel.name}</span>
+			</li>
+		{/each}
+	</ul>
+</div>
