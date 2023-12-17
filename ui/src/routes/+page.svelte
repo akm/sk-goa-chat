c202f183c808c0ede1c3e2881021d24330585ee6
akm <akm2000@gmail.com>
2023-12-17 11:26:59 +0900

    :sparkles: ListGroup を使ってチャンネル一覧を表示

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
-	import { Input, Label, Checkbox, Button } from 'flowbite-svelte';
-	import { TrashBinSolid } from 'flowbite-svelte-icons';
+	import { Listgroup } from 'flowbite-svelte';
 	import type { Channel } from '$lib/models/channel';
 	export let data: { channels: Channel[] };
+
+	const channelLinks = data.channels.map((channel) => ({
+		name: channel.name,
+		href: `/channels/${channel.id}`
+	}));
 </script>
 
 <div class="centered">
-	<h1>Channels</h1>
-
-	<ul class="todos">
-		{#each data.channels as channel (channel.id)}
-			<li>
-				<span>{channel.name}</span>
-			</li>
-		{/each}
-	</ul>
+	<Listgroup active items={channelLinks} let:item class="w-48">
+		{item.name}
+	</Listgroup>
 </div>
