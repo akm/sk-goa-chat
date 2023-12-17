efc051b245819d4e2a9269107cdb3cf1686ae016
akm <akm2000@gmail.com>
2023-12-17 18:38:11 +0900

    :sparkles: チャンネルの更新と削除を実装

--- ui/src/routes/channels/[id]/+page.svelte
+++ ui/src/routes/channels/[id]/+page.svelte
@@ -1,15 +1,47 @@
 <script lang="ts">
-	import { Heading, TextPlaceholder } from 'flowbite-svelte';
+	import { Heading, TextPlaceholder, Alert } from 'flowbite-svelte';
+	import { Button, Modal, Label, Input, Hr } from 'flowbite-svelte';
+	import { CogOutline, TrashBinSolid, InfoCircleSolid } from 'flowbite-svelte-icons';
 
 	import type { Channel } from '$lib/models/channel';
-	import { Button, Modal, Label, Input, Hr } from 'flowbite-svelte';
-	import { CogOutline, TrashBinSolid } from 'flowbite-svelte-icons';
 
 	export let data = {
 		channel: Channel
 	};
 
+	let name = data.channel.name;
+	let errorMessage = '';
+
 	let settingVisible = false;
+
+	const updateChannel = async () => {
+		const result = await fetch(`/api/channels/${data.channel.id}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ name })
+		});
+		const json = await result.json();
+		console.log('json', json);
+		if (!json.id) {
+			errorMessage = json.message;
+			return;
+		}
+		window.location.reload();
+	};
+
+	const deleteChannel = async () => {
+		const result = await fetch(`/api/channels/${data.channel.id}`, {
+			method: 'DELETE',
+			headers: { 'Content-Type': 'application/json' }
+		});
+		const json = await result.json();
+		console.log('json', json);
+		if (!json.id) {
+			errorMessage = json.message;
+			return;
+		}
+		window.location.href = '/';
+	};
 </script>
 
 <Heading tag="h3" class="mb-4">
@@ -24,14 +56,29 @@
 <TextPlaceholder class="mb-8" />
 
 <Modal bind:open={settingVisible} size="xs" autoclose={false} class="w-full">
-	<form class="flex flex-col space-y-6" action="#">
-		<h3 class="text-xl font-medium text-gray-900 dark:text-white">Channel Settings</h3>
+	<h3 class="text-xl font-medium text-gray-900 dark:text-white">Channel Settings</h3>
+
+	{#if errorMessage}
+		<Alert
+			type="error"
+			class="mb-4"
+			dismissable
+			on:close={() => {
+				errorMessage = '';
+			}}
+		>
+			<InfoCircleSolid slot="icon" class="w-4 h-4" />
+			{errorMessage}
+		</Alert>
+	{/if}
+
+	<div class="flex flex-col space-y-6">
 		<Label class="space-y-2">
 			<span>Name</span>
-			<Input type="text" name="name" placeholder={data.channel.name} required />
+			<Input type="text" name="name" required bind:value={name} />
 		</Label>
 		<div class="flex">
-			<Button type="submit" class="mr-4">Update</Button>
+			<Button type="submit" class="mr-4" on:click={updateChannel}>Update</Button>
 			<Button
 				color="alternative"
 				on:click={() => {
@@ -39,13 +86,13 @@
 				}}>Cancel</Button
 			>
 		</div>
-	</form>
+	</div>
 
 	<Hr />
 
 	<h3 class="text-xl font-medium text-red-700 dark:text-white">Danger Done</h3>
 
-	<Button>
+	<Button on:click={deleteChannel}>
 		<TrashBinSolid class="w-3.5 h-3.5 me-2" />
 		Delete this channel forever
 	</Button>
