34d333ca839f957f492b27cda3ac5c73ff3b36db
akm <akm2000@gmail.com>
2023-12-23 21:44:10 +0900

    :green_heart: DropAll でのログを t.Logf を使うように変更

--- apisvr/testlib/testsql/drop_all.go
+++ apisvr/testlib/testsql/drop_all.go
@@ -2,7 +2,6 @@ package testsql
 
 import (
 	"apisvr/lib/sql"
-	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,18 +12,19 @@ func DropAll(t *testing.T, conn *sql.DB) {
 	tableNames := SortedTableNames(t, conn)
 	errs := []error{}
 	for _, tbl := range tableNames {
-		// log.Printf("DROP TABLE\t%s\ttrying\n", tbl)
+		t.Logf("DROP TABLE\t%s\ttrying\n", tbl)
 		_, err := conn.Exec("DROP TABLE IF EXISTS " + tbl)
 		if !assert.NoError(t, err) {
-			log.Printf("DROP TABLE\t%s\tERROR\n", tbl)
+			t.Logf("DROP TABLE\t%s\tERROR\n", tbl)
 			errs = append(errs, err)
-			// } else {
-			// log.Printf("DROP TABLE\t%s\tSUCCESS\n", tbl)
+		} else {
+			t.Logf("DROP TABLE\t%s\tSUCCESS\n", tbl)
 		}
 	}
 	if len(errs) > 0 {
-		log.Printf("DROP TABLE\tERROR\n%+v\n", errs)
+		t.Logf("DROP TABLE\tERROR\n%+v\n", errs)
 		t.Fatalf("failed to drop tables: %+v", errs)
+	} else {
+		t.Logf("DROP TABLE\tCOMPLETE\n")
 	}
-	log.Printf("DROP TABLE\tCOMPLETE\n")
 }
