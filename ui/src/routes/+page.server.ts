6698ca56b4518d83d880fbcc93e8bc26b2e92385
akm <akm2000@gmail.com>
2023-12-23 12:08:41 +0900

    :sparkles: ログインせずにトップページにアクセスした場合はログイン画面にリダイレクト

--- ui/src/routes/+page.server.ts
+++ ui/src/routes/+page.server.ts
@@ -1,6 +1,8 @@
-// import type { ServerLoadEvent } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
+import type { ServerLoadEvent } from '@sveltejs/kit';
 
-// export async function load(): Promise<{ channels: Channel[] }> {
-// 	const channels = await listChannels();
-// 	return { channels };
-// }
+export async function load(event: ServerLoadEvent): Promise<void> {
+	if (!event.locals.user) {
+		throw redirect(302, '/signin');
+	}
+}
