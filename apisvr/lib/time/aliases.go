b7026b9449435d91a723e88462a422b63797e5e0
akm <akm2000@gmail.com>
2023-12-25 09:31:03 +0900

    :sparkles: time パッケージによく使う定数のエイリアスを追加

--- apisvr/lib/time/aliases.go
+++ apisvr/lib/time/aliases.go
@@ -9,3 +9,14 @@ type (
 	Duration = orig.Duration
 	Location = orig.Location
 )
+
+const (
+	Nanosecond  = orig.Nanosecond
+	Microsecond = orig.Microsecond
+	Millisecond = orig.Millisecond
+	Second      = orig.Second
+	Minute      = orig.Minute
+	Hour        = orig.Hour
+
+	RFC3339 = orig.RFC3339
+)
