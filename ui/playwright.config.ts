a63799b15fc26aa931ec55a83857f6b17ba89df5
akm <akm2000@gmail.com>
2023-12-31 18:10:19 +0900

    :wrench: Playwright に baseURL 等の必要な設定を追加

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -1,6 +1,16 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	timeout: 5 * 60_000,
+	use: {
+		headless: process.env.HEADED != 'true',
+		launchOptions: {
+			slowMo: Number(process.env.SLOMO || 0) // テスト実行時のスローモーション。デモなら 2000 くらいがよい
+		},
+		baseURL: 'http://localhost:4173',
+		trace: 'retain-on-failure'
+	},
+
 	// https://playwright.dev/docs/api/class-testconfig#test-config-web-server
 	webServer: [
 		{
