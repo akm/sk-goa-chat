1f710429caf710a204906c06c8e1c5edc8a02dcf
akm <akm2000@gmail.com>
2023-12-31 20:31:36 +0900

    :sparkles: npm run dev と npm run preview での挙動の違いを吸収

--- ui/src/lib/server/firebase-admin.ts
+++ ui/src/lib/server/firebase-admin.ts
@@ -3,7 +3,16 @@ import { initializeApp, apps, app } from 'firebase-admin';
 // FirebaseAppError: The default Firebase app already exists.
 // This means you called initializeApp() more than once without
 // providing an app name as the second argument.
-// というエラーが発生していたので単純に initializeApp() を呼ぶのではなく
-// apps.length で判定し、存在していた場合には app() を呼ぶように修正
-const adminApp = apps.length === 0 ? initializeApp() : app();
+// というエラーが npm run dev での実行時に発生していたので単純に
+// その場合は initializeApp() を呼ぶのではなく、apps.length で判定し、
+// 存在していた場合には app() を呼ぶように修正
+// See https://vitejs.dev/guide/env-and-mode
+const adminApp: app.App = ((): app.App => {
+	if (import.meta.env.DEV) {
+		return apps.length === 0 ? initializeApp() : app();
+	} else {
+		return initializeApp();
+	}
+})();
+
 export const auth = adminApp.auth();
