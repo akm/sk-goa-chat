f7212fb534f820eb44e90dd097254fd5674467cf
akm <akm2000@gmail.com>
2023-12-31 20:54:45 +0900

    :sparkles: 正しくビルドできていなかったので修正

--- ui/src/lib/server/firebase-admin.ts
+++ ui/src/lib/server/firebase-admin.ts
@@ -1,4 +1,6 @@
-import { initializeApp, apps, app } from 'firebase-admin';
+// See https://firebase.google.com/docs/admin/setup?hl=ja
+import { initializeApp, getApp, getApps } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
 
 // FirebaseAppError: The default Firebase app already exists.
 // This means you called initializeApp() more than once without
@@ -7,12 +9,12 @@ import { initializeApp, apps, app } from 'firebase-admin';
 // その場合は initializeApp() を呼ぶのではなく、apps.length で判定し、
 // 存在していた場合には app() を呼ぶように修正
 // See https://vitejs.dev/guide/env-and-mode
-const adminApp: app.App = ((): app.App => {
+const adminApp = (() => {
 	if (import.meta.env.DEV) {
-		return apps.length === 0 ? initializeApp() : app();
+		return getApps().length === 0 ? initializeApp() : getApp();
 	} else {
 		return initializeApp();
 	}
 })();
 
-export const auth = adminApp.auth();
+export const auth = getAuth(adminApp);
