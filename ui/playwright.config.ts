b4a723d0d44be1f29852de9e18a312813b4ac5c3
akm <akm2000@gmail.com>
2023-12-31 17:38:10 +0900

    :wrench: playwright のテスト実行時に apisvr と sveltekit のサーバーの両方を起動

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -1,10 +1,24 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
-	},
+	// https://playwright.dev/docs/api/class-testconfig#test-config-web-server
+	webServer: [
+		{
+			command: 'make apisvr_run',
+			port: 8001,
+			reuseExistingServer: !process.env.CI
+		},
+		{
+			command: 'npm run build && npm run preview',
+			port: 4173,
+			reuseExistingServer: !process.env.CI,
+			env: {
+				GOOGLE_CLOUD_PROJECT: 'sk-goa-chat',
+				FIREBASE_AUTH_EMULATOR_HOST: '127.0.0.1:9090'
+			}
+		}
+	],
+
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
