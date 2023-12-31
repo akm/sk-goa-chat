e29d1ec918c3e6dd98f5c874d267a01a3236b95c
akm <akm2000@gmail.com>
2023-12-31 14:49:26 +0900

    :green_heart: testjson.ReassignAs 関数を追加

--- apisvr/testlib/testjson/reassign.go
+++ apisvr/testlib/testjson/reassign.go
@@ -17,3 +17,16 @@ func Reassign[T any](t *testing.T, v *T) *T {
 	}
 	return &r
 }
+
+func ReassignAs[T1, T2 any](t *testing.T, v *T1) *T2 {
+	t.Helper()
+	b, err := json.Marshal(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var r T2
+	if err := json.Unmarshal(b, &r); err != nil {
+		t.Fatal(err)
+	}
+	return &r
+}
