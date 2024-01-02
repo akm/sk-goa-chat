8ba1904fd06ad60774455b16ba8621407ae0ef78
akm <akm2000@gmail.com>
2024-01-02 20:46:52 +0900

    :sparkles: sqldb-logger のセットアップ
    https://github.com/simukti/sqldb-logger を参照

--- apisvr/lib/sql/connect.go
+++ apisvr/lib/sql/connect.go
@@ -1,11 +1,13 @@
 package sql
 
 import (
+	"apisvr/lib/errors"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-
-	"apisvr/lib/errors"
+	"github.com/rs/zerolog"
+	sqldblogger "github.com/simukti/sqldb-logger"
+	"github.com/simukti/sqldb-logger/logadapter/zerologadapter"
 )
 
 func Open() (*DB, error) {
@@ -14,5 +16,8 @@ func Open() (*DB, error) {
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to open database with %s", dsn)
 	}
+	// See https://github.com/simukti/sqldb-logger
+	loggerAdapter := zerologadapter.New(zerolog.New(os.Stderr))
+	db = sqldblogger.OpenDriver(dsn, db.Driver(), loggerAdapter) // db is STILL *sql.DB
 	return db, nil
 }
