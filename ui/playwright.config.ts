e88c47e5da4cea0583ab90427e07a8f196cdfd63
akm <akm2000@gmail.com>
2024-01-02 18:06:04 +0900

    :wrench: サーバープロセス内の firebase-admin が使用する環境変数 GOOGLE_CLOUD_PROJECT が設定されていなければ 環境変数 VITE_GOOGLE_CLOUD_PROJECT から取得

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -24,6 +24,7 @@ const config: PlaywrightTestConfig = {
 			reuseExistingServer: !process.env.CI,
 			env: {
 				GOOGLE_CLOUD_PROJECT: 'sk-goa-chat',
+				VITE_GOOGLE_CLOUD_PROJECT: 'sk-goa-chat',
 				// https://firebase.google.com/docs/emulator-suite/connect_auth?hl=ja
 				FIREBASE_AUTH_EMULATOR_HOST: '127.0.0.1:9090',
 				VITE_FIREBASE_AUTH_EMULATOR_HOST: '127.0.0.1:9090',
