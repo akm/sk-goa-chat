251e24481d1f8597bc9052321705fd7f1d94cc57
akm <akm2000@gmail.com>
2024-01-03 16:56:48 +0900

    :wrench: make goa_example で services/cmd を一度削除するように変更

--- apisvr/Makefiles/goa.mk
+++ apisvr/Makefiles/goa.mk
@@ -13,7 +13,11 @@ goa_gen:
 	goa gen $(GOA_ROOT_PACKAGE)/design -o $(GOA_SERVICES_DIR) && \
 	$(MAKE) -C ../modifiers apisvr_services_gen
 
+.PHONY: services_cmd_remove
+services_cmd_remove:
+	rm -rf $(GOA_SERVICES_DIR)/cmd
+
 .PHONY: goa_example
-goa_example:
+goa_example: services_cmd_remove
 	goa example $(GOA_ROOT_PACKAGE)/design -o $(GOA_SERVICES_DIR) && \
 	$(MAKE) -C ../modifiers apisvr_services_cmd
