8709dc6d1080e6385c27997d27ecf897875babf4
akm <akm2000@gmail.com>
2024-01-05 08:49:27 +0900

    :sparkles: userssrvc を実装

--- apisvr/services/users.go
+++ apisvr/services/users.go
@@ -1,35 +1,101 @@
 package chatapi
 
 import (
+	"apisvr/lib/errors"
+	"apisvr/lib/sql"
 	"apisvr/models"
 	log "apisvr/services/gen/log"
 	users "apisvr/services/gen/users"
 	"context"
+	"fmt"
 	"time"
+
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 // users service example implementation.
 // The example methods log the requests and return zero values.
 type userssrvc struct {
 	logger *log.Logger
+	*UsersConvertor
 }
 
 // NewUsers returns the users service implementation.
 func NewUsers(logger *log.Logger) users.Service {
-	return &userssrvc{logger}
+	return &userssrvc{logger: logger, UsersConvertor: NewUsersConvertor()}
+}
+
+func (s *userssrvc) sqlOpen() (*sql.DB, error) {
+	return sql.Open(s.logger.Logger)
 }
 
 // List implements list.
 func (s *userssrvc) List(ctx context.Context) (res *users.UserList, err error) {
-	res = &users.UserList{}
 	s.logger.Info().Msg("users.list")
+	ctx = SetupContext(ctx)
+	db, err := s.sqlOpen()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	results, err := models.Users().All(ctx, db)
+	if err != nil {
+		return nil, err
+	}
+
+	res = s.ModelsToList(results)
 	return
 }
 
 // Create implements create.
 func (s *userssrvc) Create(ctx context.Context, p *users.UserCreatePayload) (res *users.User, err error) {
-	res = &users.User{}
 	s.logger.Info().Msg("users.create")
+
+	if p.Name == "" {
+		return nil, users.MakeInvalidPayload(fmt.Errorf("name is required"))
+	} else {
+		runes := []rune(p.Name)
+		if len(runes) > 255 {
+			return nil, users.MakeInvalidPayload(fmt.Errorf("name is too long"))
+		}
+	}
+	if p.Email == "" {
+		return nil, users.MakeInvalidPayload(fmt.Errorf("email is required"))
+	} else {
+		runes := []rune(p.Email)
+		if len(runes) > 255 {
+			return nil, users.MakeInvalidPayload(fmt.Errorf("email is too long"))
+		}
+	}
+
+	ctx = SetupContext(ctx)
+	db, err := s.sqlOpen()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	if user, err := models.Users(qm.Where("email = ?", p.Email)).One(ctx, db); err != nil {
+		if err == sql.ErrNoRows {
+			// OK
+		} else {
+			return nil, errors.Wrapf(err, "failed to query user by email: %s", p.Email)
+		}
+	} else {
+		return s.ModelToResult(user), nil
+	}
+
+	m := &models.User{
+		Name:  p.Name,
+		Email: p.Email,
+	}
+	if err := m.Insert(ctx, db, boil.Infer()); err != nil {
+		return nil, err
+	}
+
+	res = s.ModelToResult(m)
 	return
 }
 
