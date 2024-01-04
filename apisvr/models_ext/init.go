f3369ef38435facc7993a27189bcfdde9bad880c
akm <akm2000@gmail.com>
2024-01-05 08:42:16 +0900

    :sparkles: Channel の Update と Userの Insert と Update の Hook を登録

--- apisvr/models_ext/init.go
+++ apisvr/models_ext/init.go
@@ -9,10 +9,27 @@ import (
 )
 
 func init() {
+	// Channel
 	models.AddChannelHook(boil.BeforeInsertHook, func(ctx context.Context, exec boil.ContextExecutor, p *models.Channel) error {
 		now := time.Now()
 		p.CreatedAt = now
 		p.UpdatedAt = now
+		return nil
+	})
+	models.AddChannelHook(boil.BeforeUpdateHook, func(ctx context.Context, exec boil.ContextExecutor, p *models.Channel) error {
+		p.UpdatedAt = time.Now()
+		return nil
+	})
+
+	// User
+	models.AddUserHook(boil.BeforeInsertHook, func(ctx context.Context, exec boil.ContextExecutor, p *models.User) error {
+		now := time.Now()
+		p.CreatedAt = now
+		p.UpdatedAt = now
+		return nil
+	})
+	models.AddUserHook(boil.BeforeUpdateHook, func(ctx context.Context, exec boil.ContextExecutor, p *models.User) error {
+		p.UpdatedAt = time.Now()
 		return nil
 	})
 }
