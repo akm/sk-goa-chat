2d677bd79fe913d7ef3b2b057a4e4957d2914710
akm <akm2000@gmail.com>
2024-01-06 14:39:15 +0900

    :green_heart: テスト時に firebase emulator に接続して検証するための ID Tokenを取得するための関数 GetIdToken を追加

---
+++ apisvr/testlib/testgoogle/testidentitytoolkit/id_token.go
@@ -0,0 +1,39 @@
+package testidentitytoolkit
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"google.golang.org/api/identitytoolkit/v1"
+	"google.golang.org/api/option"
+)
+
+// See
+// https://zenn.dev/nbstsh/scraps/421f07c3e01c29
+// https://pkg.go.dev/google.golang.org/api@v0.155.0/identitytoolkit/v1#AccountsService.SignInWithPassword
+func GetIdToken(t testing.TB, ctx context.Context, email, password string, opts ...option.ClientOption) string {
+	if len(opts) == 0 {
+		opts = []option.ClientOption{
+			option.WithEndpoint(fmt.Sprintf("http://localhost:%s/identitytoolkit.googleapis.com/v1", os.Getenv("FIREBASE_AUTH_EMULATOR_HOST"))),
+		}
+	}
+	// option.WithEndpoint("http://localhost:9099/identitytoolkit.googleapis.com/v1"),
+	// option.WithAPIKey("AIzaSyBRcOfsczLRC7VX0iirMU2dlAFKYPRo-9U"),
+	identitytoolkitService, err := identitytoolkit.NewService(ctx, opts...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	call := identitytoolkitService.Accounts.SignInWithPassword(&identitytoolkit.GoogleCloudIdentitytoolkitV1SignInWithPasswordRequest{
+		Email:             email,
+		Password:          password,
+		ReturnSecureToken: true,
+	})
+	res, err := call.Do()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return res.IdToken
+}
