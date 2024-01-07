693421682975da79c63ebeee26e22561c8122c6c
akm <akm2000@gmail.com>
2024-01-07 20:47:49 +0900

    :sparkles: gRPC での呼び出し時にsessionIdを指定

--- ui/src/routes/+layout.server.ts
+++ ui/src/routes/+layout.server.ts
@@ -3,7 +3,7 @@ import { listChannels } from '$lib/server/grpc_channels_client';
 
 // get `locals.user` and pass it to the `page` store
 export const load: LayoutServerLoad = async ({ locals }) => {
-	const channels = await listChannels();
+	const channels = locals.sessionID ? await listChannels({ sessionId: locals.sessionID }) : [];
 	return {
 		user: locals.user,
 		channels
