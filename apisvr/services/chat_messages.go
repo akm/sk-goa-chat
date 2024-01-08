daf864ee69cb1536360101813372439d0af09781
akm <akm2000@gmail.com>
2024-01-09 08:44:15 +0900

    :sparkles: 基本的なCRUDを実装

--- apisvr/services/chat_messages.go
+++ apisvr/services/chat_messages.go
@@ -1,56 +1,137 @@
 package chatapi
 
 import (
+	"apisvr/lib/sql"
 	"apisvr/lib/time"
 	"apisvr/models"
 	chatmessages "apisvr/services/gen/chat_messages"
 	log "apisvr/services/gen/log"
 	"context"
+	"fmt"
+
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 // chat_messages service example implementation.
 // The example methods log the requests and return zero values.
 type chatMessagessrvc struct {
-	logger *log.Logger
+	baseAuthService
+	*ChatMessageConvertor
 }
 
 // NewChatMessages returns the chat_messages service implementation.
 func NewChatMessages(logger *log.Logger) chatmessages.Service {
-	return &chatMessagessrvc{logger}
+	return &chatMessagessrvc{baseAuthService: newBaseAuthService(logger), ChatMessageConvertor: NewChatMessageConvertor()}
 }
 
 // List implements list.
 func (s *chatMessagessrvc) List(ctx context.Context, p *chatmessages.ListPayload) (res *chatmessages.ChatMessageList, err error) {
-	res = &chatmessages.ChatMessageList{}
-	s.logger.Info().Msg("chatMessages.list")
+	err = s.actionWithAuth(ctx, "chatMessages.list", p.SessionID, func(ctx context.Context, db *sql.DB, user *models.User) error {
+		results, err := models.ChatMessages().All(ctx, db)
+		if err != nil {
+			return err
+		}
+		res = s.ModelsToList(results)
+		return nil
+	})
 	return
 }
 
 // Show implements show.
 func (s *chatMessagessrvc) Show(ctx context.Context, p *chatmessages.ShowPayload) (res *chatmessages.ChatMessage, err error) {
-	res = &chatmessages.ChatMessage{}
-	s.logger.Info().Msg("chatMessages.show")
+	err = s.actionWithAuth(ctx, "chatMessages.show", p.SessionID, func(ctx context.Context, db *sql.DB, user *models.User) error {
+		m, err := models.FindChatMessage(ctx, db, p.ID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return chatmessages.MakeNotFound(fmt.Errorf("chat message not found"))
+			}
+			return err
+		}
+		res = s.ModelToResult(m)
+		return nil
+	})
 	return
 }
 
 // Create implements create.
+const maxContentLength = 16_777_215
+
 func (s *chatMessagessrvc) Create(ctx context.Context, p *chatmessages.ChatMessageCreatePayload) (res *chatmessages.ChatMessage, err error) {
-	res = &chatmessages.ChatMessage{}
-	s.logger.Info().Msg("chatMessages.create")
+	err = s.actionWithAuth(ctx, "chatMessages.create", p.SessionID, func(ctx context.Context, db *sql.DB, user *models.User) error {
+		if p.Content == "" {
+			return chatmessages.MakeInvalidPayload(fmt.Errorf("content is required"))
+		} else {
+			runes := []rune(p.Content)
+			if len(runes) > maxContentLength {
+				return chatmessages.MakeInvalidPayload(fmt.Errorf("content is too long"))
+			}
+		}
+		ch, err := models.FindChannel(ctx, db, p.ChannelID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return chatmessages.MakeInvalidPayload(fmt.Errorf("channel not found"))
+			}
+		}
+		m := &models.ChatMessage{
+			ChannelID: ch.ID,
+			UserID:    null.Uint64From(user.ID),
+			UserName:  user.Name,
+			Content:   p.Content,
+		}
+		if err := m.Insert(ctx, db, boil.Infer()); err != nil {
+			return err
+		}
+		res = s.ModelToResult(m)
+		return nil
+	})
 	return
 }
 
 // Update implements update.
 func (s *chatMessagessrvc) Update(ctx context.Context, p *chatmessages.ChatMessageUpdatePayload) (res *chatmessages.ChatMessage, err error) {
-	res = &chatmessages.ChatMessage{}
-	s.logger.Info().Msg("chatMessages.update")
+	err = s.actionWithAuth(ctx, "chatMessages.update", p.SessionID, func(ctx context.Context, db *sql.DB, user *models.User) error {
+		if p.Content == "" {
+			return chatmessages.MakeInvalidPayload(fmt.Errorf("content is required"))
+		} else {
+			runes := []rune(p.Content)
+			if len(runes) > maxContentLength {
+				return chatmessages.MakeInvalidPayload(fmt.Errorf("content is too long"))
+			}
+		}
+		m, err := models.FindChatMessage(ctx, db, p.ID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return chatmessages.MakeNotFound(fmt.Errorf("chat message not found"))
+			}
+			return err
+		}
+		m.Content = p.Content
+		if _, err := m.Update(ctx, db, boil.Infer()); err != nil {
+			return err
+		}
+		res = s.ModelToResult(m)
+		return nil
+	})
 	return
 }
 
 // Delete implements delete.
 func (s *chatMessagessrvc) Delete(ctx context.Context, p *chatmessages.DeletePayload) (res *chatmessages.ChatMessage, err error) {
-	res = &chatmessages.ChatMessage{}
-	s.logger.Info().Msg("chatMessages.delete")
+	err = s.actionWithAuth(ctx, "chatMessages.delete", p.SessionID, func(ctx context.Context, db *sql.DB, user *models.User) error {
+		m, err := models.FindChatMessage(ctx, db, p.ID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return chatmessages.MakeNotFound(fmt.Errorf("chat message not found"))
+			}
+			return err
+		}
+		if _, err := m.Delete(ctx, db); err != nil {
+			return err
+		}
+		res = s.ModelToResult(m)
+		return nil
+	})
 	return
 }
 
