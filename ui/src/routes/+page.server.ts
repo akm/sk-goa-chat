1bc340b389d56663e3ab6c401e7eaea4f421005c
akm <akm2000@gmail.com>
2024-01-13 10:44:28 +0900

    :sparkles: ログイン時にルートを表示しようとすると最初のチャンネルにリダイレクト

--- ui/src/routes/+page.server.ts
+++ ui/src/routes/+page.server.ts
@@ -1,8 +1,16 @@
 import { redirect } from '@sveltejs/kit';
 import type { ServerLoadEvent } from '@sveltejs/kit';
+import { listChannels } from '$lib/server/grpc_channels_client';
 
 export async function load(event: ServerLoadEvent): Promise<void> {
 	if (!event.locals.user) {
 		throw redirect(302, '/signin');
+	}
+
+	const channels = event.locals.sessionID
+		? await listChannels({ sessionId: event.locals.sessionID })
+		: undefined;
+	if (channels && channels.length > 0) {
+		throw redirect(302, `/channels/${channels[0].id}`);
 	}
 }
