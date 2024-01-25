37030f34923183ad8e1804758a9ed5e131bb9497
akm <akm2000@gmail.com>
2024-01-25 10:14:44 +0900

    :robot: CORSの AllowHeaders と AllowCredentials を設定

--- modifiers/apisvr/services/cmd/apisvr/http.go.rb
+++ modifiers/apisvr/services/cmd/apisvr/http.go.rb
@@ -2,5 +2,12 @@
 
 $content.sub!('"net/http"'){|key| ['"apisvr/lib/http/cors"', "\t"+ key].join("\n") }
 $content.sub!('handler = httpmdlwr.Log(adapter)(handler)') do |s|
-    ['handler = cors.NewFromEnv("APP_CORS_ALLOW_ORIGINS").Handle(handler)', s].join("\n")
+    [   
+        'handler = cors.NewFromEnv("APP_CORS_ALLOW_ORIGINS").Tap(func(c *cors.Cors) {',
+        '    c.AllowHeaders = []string{"Content-Type", "Cookie"}', 
+        '    c.AllowCredentials = "true"', 
+        '    c.Logger = logger.Logger', 
+        '}).Handle(handler)',
+        s,
+    ].join("\n")
 end
