65c61d625151774e57b266723618d3577d023f64
akm <akm2000@gmail.com>
2024-01-26 09:55:05 +0900

    :wrench: apisvr の方の sqlboiler.mk は svrlib のmakeタスクを呼び出すだけに変更

--- apisvr/Makefiles/sqlboiler.mk
+++ apisvr/Makefiles/sqlboiler.mk
@@ -1,7 +1,3 @@
 .PHONY: sqlboiler_gen
-sqlboiler_gen: sqlboiler_gen_prepare
-	sqlboiler mysql
-
-.PHONY: sqlboiler_gen_prepare
-sqlboiler_gen_prepare:
-	$(MAKE) -C ../dbmigrations up
+sqlboiler_gen:
+	$(MAKE) -C ../svrlib sqlboiler_gen
