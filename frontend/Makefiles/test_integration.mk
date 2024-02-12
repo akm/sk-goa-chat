156298614e6166bf59c31dc0434a48f3c4b7729c
akm <akm2000@gmail.com>
2024-02-12 09:49:13 +0900

    :wrench: playwright.config.ts に config.mk のポート番号が反映されるように

--- frontend/Makefiles/test_integration.mk
+++ frontend/Makefiles/test_integration.mk
@@ -1,9 +1,13 @@
+PLAYWRIGHT_ENVS=\
+	APP_UISVR_HTTP_PORT=$(APP_PORT_UISVR_HTTP_e2e_test) \
+	APP_APISVR_HTTP_PORT=$(APP_PORT_APISVR_HTTP_e2e_test)
+
 .PHONY: test_integration
 test_integration: test_integration_setup test_integration_run
 
 .PHONY: test_integration_run
 test_integration_run:
-	npx playwright test tests/integration/*.scenario.ts
+	$(PLAYWRIGHT_ENVS) npx playwright test tests/integration/*.scenario.ts
 
 .PHONY: test_integration_setup
 test_integration_setup: test_integration_containers_restart test_integration_dbmigrations_up
