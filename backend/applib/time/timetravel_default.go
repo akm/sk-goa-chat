f2a153843451436c192adf11ba8c635be4b4297e
akm <akm2000@gmail.com>
2024-02-12 20:29:44 +0900

    :green_heart: :recycle: applib/time/timetest  パッケージを抽出

--- backend/applib/time/timetravel_default.go
+++ backend/applib/time/timetravel_default.go
@@ -4,19 +4,8 @@ package time
 
 import (
 	"fmt"
-	orig "time"
 )
 
-var nowFunc = orig.Now
-
 func SetNowFunc(f func() Time) func() {
-	panic(fmt.Errorf("timetravel is not enabled"))
-}
-
-func SetTime(t Time) func() {
-	panic(fmt.Errorf("timetravel is not enabled"))
-}
-
-func ClearNanoSec(t Time) Time {
 	panic(fmt.Errorf("timetravel is not enabled"))
 }
