a58e7527f4a9b81b72c11a298629753d171bbfd6
akm <akm2000@gmail.com>
2024-02-13 22:02:28 +0900

    :sparkles: e2e_test ではdbmigrations でデモ用のデータを投入するように

--- backend/dbmigrations/helpers/localdev.go
+++ backend/dbmigrations/helpers/localdev.go
@@ -9,6 +9,8 @@ var DemoData = NewCondition(func() bool {
 	if os.Getenv("DEMO_DATA") != "" {
 		return os.Getenv("DEMO_DATA") == "true"
 	}
+	appEnv := os.Getenv("APP_ENV")
 	return os.Getenv("APP_STAGE_TYPE") == "local" &&
-		!strings.Contains(os.Getenv("APP_ENV"), "test")
+		!strings.Contains(appEnv, "test") ||
+		appEnv == "e2e_test"
 })
