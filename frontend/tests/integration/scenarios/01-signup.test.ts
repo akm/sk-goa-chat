0e8dd978b9842de0f98034857f6b6df74050d0bd
akm <akm2000@gmail.com>
2024-02-13 21:58:14 +0900

    :sparkles: サインインした後にCookieをファイルにダンプ

--- frontend/tests/integration/scenarios/01-signup.test.ts
+++ frontend/tests/integration/scenarios/01-signup.test.ts
@@ -1,15 +1,13 @@
 import { test } from '@playwright/test';
 import { signup } from '../steps/signup';
 import { signin, signout } from '../steps/signin';
+import { saveCookiesTo } from '../steps/auth';
+import { foo } from './config';
 
-test('show signin page when go to root', async ({ page }) => {
-	const foo = {
-		email: 'foo@example.com',
-		password: 'Passw0rd!',
-		name: 'Foo'
-	};
-
+test('show signin page when go to root', async ({ page, context }) => {
 	await signup(page, foo);
 	await signout(page);
 	await signin(page, foo);
+
+	await saveCookiesTo(context, foo.cookieFile);
 });
---
+++ frontend/tests/integration/scenarios/config.ts
@@ -0,0 +1,6 @@
+export const foo = {
+	email: 'foo@example.com',
+	password: 'Passw0rd!',
+	name: 'Foo',
+	cookieFile: 'tests/integration/tmp/cookies-foo.json'
+};
