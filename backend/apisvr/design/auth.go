9609a87be556750de4f67b46be62429480a225d7
akm <akm2000@gmail.com>
2024-02-25 21:10:41 +0900

    :page_with_curl: authApiKeyField で定義するフィールドを id_token に変更

--- backend/apisvr/design/auth.go
+++ backend/apisvr/design/auth.go
@@ -4,13 +4,13 @@ import "goa.design/goa/v3/dsl"
 
 //nolint:unused
 const authApiKeyScheme = "api_key"
-const authApiKeyName = "auth_api_key"
+const authApiKeyName = "id_token"
 
 //nolint:unused
 var authApiKeySecurity = dsl.APIKeySecurity(authApiKeyScheme, func() {
 })
 
 func authApiKeyField(tag any) string {
-	dsl.APIKeyField(tag, authApiKeyScheme, authApiKeyName, dsl.String, "Auth API Key")
+	dsl.APIKeyField(tag, authApiKeyScheme, authApiKeyName, dsl.String, "X-ID-TOKEN", func() { dsl.Example("abcdef12345") })
 	return authApiKeyName
 }
