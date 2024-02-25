e69cc7c5d4e78e257c09b7af40599506fb785db7
akm <akm2000@gmail.com>
2024-02-25 20:26:41 +0900

    :page_with_curl: session という文字列を使っている箇所を改善

--- backend/apisvr/design/auth.go
+++ backend/apisvr/design/auth.go
@@ -4,14 +4,13 @@ import "goa.design/goa/v3/dsl"
 
 //nolint:unused
 const authApiKeyScheme = "api_key"
-const authApiKeyName = "session_id"
+const authApiKeyName = "auth_api_key"
 
 //nolint:unused
 var authApiKeySecurity = dsl.APIKeySecurity(authApiKeyScheme, func() {
 })
 
 func authApiKeyField(tag any) string {
-	// dsl.APIKeyField(tag, sessionIdScheme, sessionIdKey, dsl.String, "Session ID")
-	dsl.Field(tag, authApiKeyName, dsl.String, "Session ID")
+	dsl.APIKeyField(tag, authApiKeyScheme, authApiKeyName, dsl.String, "Auth API Key")
 	return authApiKeyName
 }
