f02d2f63e5e8a6c0bcecbb6e0f0da93a74dc4779
akm <akm2000@gmail.com>
2024-03-02 09:45:45 +0900

    :pencil2: setPersistence を使おうとしたらうまくいかなかったのでコメントを記録

--- frontend/src/lib/firebase/auth/index.ts
+++ frontend/src/lib/firebase/auth/index.ts
@@ -1,7 +1,14 @@
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import {
+	getAuth,
+	connectAuthEmulator
+	// setPersistence,
+	// browserLocalPersistence
+} from 'firebase/auth';
 import { app } from '../app';
 
 export const auth = getAuth(app);
+// これを行うと、ユーザーがブラウザを閉じてもログイン状態が維持されるはずなんですが、うまく機能してくれませんでした。
+// setPersistence(auth, browserLocalPersistence);
 
 // https://firebase.google.com/docs/emulator-suite/connect_auth?hl=ja#web-modular-api
 if (import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST) {
