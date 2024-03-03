2844cc05336126df94d7eb011b76655393922d21
akm <akm2000@gmail.com>
2024-03-03 20:29:18 +0900

    :sparkles: 認証情報をファイルに記録せず、都度ログインするように変更

--- frontend/tests/integration/scenarios/01-signup.test.ts
+++ frontend/tests/integration/scenarios/01-signup.test.ts
@@ -1,13 +1,10 @@
 import { test } from '@playwright/test';
 import { signup } from '../steps/signup';
 import { signin, signout } from '../steps/signin';
-import { saveCookiesTo } from '../steps/auth';
 import { foo } from './config';
 
-test('show signin page when go to root', async ({ page, context }) => {
+test('show signin page when go to root', async ({ page }) => {
 	await signup(page, foo);
 	await signout(page);
 	await signin(page, foo);
-
-	await saveCookiesTo(context, foo.cookieFile);
 });
