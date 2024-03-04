0fb2c7f35dad6a6cccf4fd0e1efbc6bdfd8a449a
akm <akm2000@gmail.com>
2024-03-05 08:01:49 +0900

    :green_heart: それぞれの project で使用するチャットを切り替える

--- frontend/tests/integration/scenarios/03-chat.test.ts
+++ frontend/tests/integration/scenarios/03-chat.test.ts
@@ -4,6 +4,7 @@ import { getFoo, getBar } from './config';
 
 import { signup } from '../steps/signup';
 import { ChannelListPane } from '../pom/panes/channel_list';
+import { NewChannelPage } from '../pom/pages/new_channel_page';
 import { ChatPage } from '../pom/pages/chat_page';
 import { signin } from '../steps/signin';
 
@@ -14,14 +15,23 @@ test('show signin page when go to root', async ({ page, browser }, workers) => {
 	await page.goto('/');
 	await signin(page, foo);
 
-	const channelList = new ChannelListPane(page);
+	const channnelName = `channel-${workers.workerIndex}`;
+	const newChannelPage = new NewChannelPage(page);
+	await test.step('チャットテスト用チャンネルの追加', async () => {
+		const chatPage = new ChatPage(page);
+		const channelList = new ChannelListPane(page);
+		await channelList.button('New Channel').click();
+		await expect(page).toHaveURL(/channels\/new$/);
+		await expect(newChannelPage.title).toBeVisible();
+		await newChannelPage.inputField('Name').fill(channnelName);
+		await newChannelPage.button('Create').click();
+		await expect(chatPage.title(channnelName)).toBeVisible();
+		await expect(channelList.list.itemByName(channnelName)).toBeVisible();
 
-	const chatPage1 = new ChatPage(page);
-	await test.step('デフォルトのチャンネルの確認', async () => {
-		await page.goto('/');
 		await expect(channelList.list.itemByName('general')).toBeVisible();
 		await expect(channelList.list.itemByName('random')).toBeVisible();
-		await expect(chatPage1.title('general')).toBeVisible();
+		await expect(channelList.list.itemByName(channnelName)).toBeVisible();
+		await expect(chatPage.title(channnelName)).toBeVisible();
 	});
 
 	const context2 = await browser.newContext();
@@ -33,10 +43,16 @@ test('show signin page when go to root', async ({ page, browser }, workers) => {
 
 		await page.waitForTimeout(1_000); // TODO このスリープを除去
 
+		const channelList = new ChannelListPane(page2);
 		await expect(channelList.list.itemByName('general')).toBeVisible();
 		await expect(channelList.list.itemByName('random')).toBeVisible();
+		await expect(channelList.list.itemByName(channnelName)).toBeVisible();
 		await expect(chatPage2.title('general')).toBeVisible();
+		await channelList.list.itemByName(channnelName).click();
+		await expect(chatPage2.title(channnelName)).toBeVisible();
 	});
+
+	const chatPage1 = new ChatPage(page);
 
 	await test.step('foo sends messages', async () => {
 		const fooMsg1 = 'foo message 1';
