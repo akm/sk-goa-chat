160277e115c226ac20b5e044ac3ad8cc04e384ed
akm <akm2000@gmail.com>
2024-03-10 01:46:26 +0900

    :wrench: 差分が存在する場合にはそれを表示

--- servers/apisvr/Makefiles/git_check.mk
+++ servers/apisvr/Makefiles/git_check.mk
@@ -5,7 +5,7 @@ git_check: git_check_uncommited_changes git_check_untracked_files
 git_check_uncommited_changes:
 	@git diff --exit-code > /dev/null && \
 		echo "no uncommited changes" || \
-		( echo "uncommited changes exists" && exit 1 )
+		( echo "uncommited changes exists" && git diff && exit 1 )
 
 GIT_UNTRACKED_FILES := $(shell git ls-files . --exclude-standard --others)
 
@@ -14,5 +14,5 @@ git_check_untracked_files:
 	@if [ "$(GIT_UNTRACKED_FILES)" = "" ]; then \
 	  echo "No untracked file" ; \
 	else \
-	  echo "There is untracked file(s): $(GIT_UNTRACKED_FILES)" && exit 1 ; \
+	  echo "There is untracked file(s): $(GIT_UNTRACKED_FILES)" && git status && exit 1 ; \
 	fi
