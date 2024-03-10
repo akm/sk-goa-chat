96650343bace6b2c13744327bfc8b794cd785972
akm <akm2000@gmail.com>
2024-03-10 20:48:47 +0900

    :sparkles: golangci-lint をインストール時に必要に応じて asdf reshim を実行

--- vendor/fukowl/makefiles/golang/lint.atom.mk
+++ vendor/fukowl/makefiles/golang/lint.atom.mk
@@ -3,7 +3,8 @@
 LINT_GOLANGCI_LINT_VERSION=v1.55.2
 LINT_GOLANGCI_LINT=$(shell go env GOPATH)/bin/golangci-lint
 $(LINT_GOLANGCI_LINT):
-	go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(LINT_GOLANGCI_LINT_VERSION)
+	go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(LINT_GOLANGCI_LINT_VERSION) && \
+	$(MAKE) asdf_reshim
 
 .PHONY: lint
 lint: $(LINT_GOLANGCI_LINT)
