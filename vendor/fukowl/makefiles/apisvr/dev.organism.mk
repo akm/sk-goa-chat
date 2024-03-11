2242722cae0581cb2caca14d6b953fbdc36c7ae6
akm <akm2000@gmail.com>
2024-03-12 07:44:04 +0900

    :wrench: :recycle: dev.organism.mk に指定する環境変数を最低限に

--- vendor/fukowl/makefiles/apisvr/dev.organism.mk
+++ vendor/fukowl/makefiles/apisvr/dev.organism.mk
@@ -1,11 +1,19 @@
+DEV_MYSQL_DSN?='$(shell $(MAKE) -C $(PATH_TO_MYSQL) --no-print-directory dsn)'
+
+DEV_SERVER_ENVS_BASE=\
+	APP_STAGE=$(APP_STAGE) \
+	APP_MYSQL_DSN=$(DEV_MYSQL_DSN) \
+	APP_LOG_CONSOLE_WRITER=true \
+	FIREBASE_AUTH_EMULATOR_HOST="127.0.0.1:$(APP_PORT_FIREBASE_AUTH_dev)"
+
 .PHONY: dev
 dev: dev_containers_up
-	$(DEV_SERVER_ENVS) $(MAKE) run_with_debug
+	$(DEV_SERVER_ENVS) $(DEV_SERVER_ENVS_BASE) $(MAKE) run_with_debug
 
 .PHONY: dev_containers_up
 dev_containers_up:
-	$(DEV_SERVER_ENVS) $(MAKE) -C $(PATH_TO_LOCALDEV) up
+	$(DEV_SERVER_ENVS) $(DEV_SERVER_ENVS_BASE) $(MAKE) -C $(PATH_TO_LOCALDEV) up
 
 .PHONY: dev_containers_down
 dev_containers_down:
-	$(DEV_SERVER_ENVS) $(MAKE) -C $(PATH_TO_LOCALDEV) down
+	$(DEV_SERVER_ENVS) $(DEV_SERVER_ENVS_BASE) $(MAKE) -C $(PATH_TO_LOCALDEV) down
