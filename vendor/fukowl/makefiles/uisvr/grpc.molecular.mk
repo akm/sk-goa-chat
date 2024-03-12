2c582bf79fd227032c528e7a9647c8cb1dc8bff6
akm <akm2000@gmail.com>
2024-03-12 09:48:35 +0900

    :wrench: :recycle: apisvr の grpc.mk を fukowl に移動

---
+++ vendor/fukowl/makefiles/uisvr/grpc.molecular.mk
@@ -0,0 +1,13 @@
+.PHONY: grpc_gen
+grpc_gen:
+	for resource in $(GRPC_RESOURCES); do \
+		RESOURCE=$${resource} $(MAKE) grpc_gen_impl; \
+	done
+
+.PHONY: grpc_gen_impl
+grpc_gen_impl:
+	mkdir -p $(PATH_TO_GRPC_DEST_DIR) && \
+	protoc \
+		--ts_out $(PATH_TO_GRPC_DEST_DIR) \
+		--proto_path $(PATH_TO_GRPC_SOURCE_DIR)/$(RESOURCE)/pb \
+		$(PATH_TO_GRPC_SOURCE_DIR)/$(RESOURCE)/pb/goagen_services_$(RESOURCE).proto
