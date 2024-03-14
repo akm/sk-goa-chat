347d4822c3a678833dc8427007455973c2e592b3
akm <akm2000@gmail.com>
2024-03-15 08:30:43 +0900

    :wrench: clients/uisvr/tests/integration/containers/Makefile に make reup を追加し、uisvrのMakefileから関連するタスクを削除

--- clients/uisvr/Makefiles/test_integration.mk
+++ clients/uisvr/Makefiles/test_integration.mk
@@ -3,39 +3,20 @@ PLAYWRIGHT_ENVS=\
 	APP_APISVR_HTTP_PORT=$(APP_PORT_APISVR_HTTP_e2e_test)
 
 .PHONY: test_integration
-test_integration: test_integration_setup test_integration_run
+test_integration: test_integration_up test_integration_run
 
 .PHONY: test_integration_run
-test_integration_run: test_integration_setup
+test_integration_run: test_integration_up
 	$(PLAYWRIGHT_ENVS) npx playwright test tests/integration/scenarios/*.test.ts
 
-.PHONY: test_integration_setup
-test_integration_setup: test_integration_containers_restart test_integration_dbmigrations_up
-
-.PHONY: test_integration_containers_up
-test_integration_containers_up:
-	$(MAKE) -C $(PATH_TO_TEST_INTEGRATION_CONTAINERS) up
-
-.PHONY: test_integration_containers_down
-test_integration_containers_down:
-	$(MAKE) -C $(PATH_TO_TEST_INTEGRATION_CONTAINERS) down
-
-.PHONY: test_integration_containers_restart
-test_integration_containers_restart:
-	$(MAKE) -C $(PATH_TO_TEST_INTEGRATION_CONTAINERS) down up
-
-.PHONY: test_integration_dbmigrations_up
-test_integration_dbmigrations_up:
-	$(MAKE) -C $(PATH_TO_TEST_INTEGRATION) mysql_wait_to_connect dbmigrations_up
-
-.PHONY: test_integration_setup
-test_integration_setup:
+.PHONY: test_integration_up
+test_integration_up: 
 	$(MAKE) -C $(PATH_TO_TEST_INTEGRATION) test_setup
+	$(MAKE) -C $(PATH_TO_TEST_INTEGRATION_CONTAINERS) reup
 
 # このターゲットは、playwright のセットアップを行うためのものです。
 # 実行すると最新の　playwright のブラウザがインストールされて時間がかかるので、
-# test_integration_setup　には含めません。
+# test_integration_up　には含めません。
 .PHONY: test_integration_pw_setup
 test_integration_pw_setup:
 	npx playwright install
-
