e8f7c1d616ee81ccacb6eeb434a58e99199b0dfa
akm <akm2000@gmail.com>
2024-03-16 13:53:46 +0900

    :wrench: :shower: dev_containers_up / _down を削除
    dev で起動するためには不要なので。

--- clients/uisvr/Makefiles/dev.mk
+++ clients/uisvr/Makefiles/dev.mk
@@ -17,13 +17,3 @@ dev_servers:
 .PHONY: dev_run
 dev_run:
 	$(DEV_ENVS) npx vite dev
-
-## TODO PATH_TO_APISVR ではなく PATH_TO_SERVERS に変更 (servers/Makefile に dev_containers_up と dev_containers_down を追加)
-
-.PHONY: dev_containers_up
-dev_containers_up:
-	$(MAKE) -C $(PATH_TO_APISVR) dev_containers_up
-
-.PHONY: dev_containers_down
-dev_containers_down:
-	$(MAKE) -C $(PATH_TO_APISVR) dev_containers_down
