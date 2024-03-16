35469b31d692176adc6fbcbeb9ad740bc609297a
akm <akm2000@gmail.com>
2024-03-16 18:20:56 +0900

    :wrench: LINT_GOLANGCI_LINT を上書き可能に

--- vendor/fukowl/makefiles/golang/lint.mk
+++ vendor/fukowl/makefiles/golang/lint.mk
@@ -1,6 +1,8 @@
 # https://github.com/golangci/golangci-lint
 # See https://golangci-lint.run/usage/false-positives/#nolint-directive for nolint directive
+ifndef LINT_GOLANGCI_LINT_VERSION
 LINT_GOLANGCI_LINT_VERSION=v1.55.2
+endif
 LINT_GOLANGCI_LINT=$(shell go env GOPATH)/bin/golangci-lint
 $(LINT_GOLANGCI_LINT):
 	go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(LINT_GOLANGCI_LINT_VERSION) && \
