e09806d6b8f040d1a2d8db3b43712ad1c61a060f
akm <akm2000@gmail.com>
2024-03-17 15:14:32 +0900

    :recycle: replace_file.rb を追加し fukowl を使わないように

--- servers/replacements/apisvr/services/gen/log/logger.go.rb
+++ servers/replacements/apisvr/services/gen/log/logger.go.rb
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-require 'fukowl'
+require './replace_file'
 
-Fukowl.replace(__FILE__.sub('replacements/', '').sub(/\.rb$/, '')) do |content|
+replace_file(__FILE__.sub('replacements/', '').sub(/\.rb$/, '')) do |content|
     content.sub!('"github.com/rs/zerolog"'){ '"applib/log"' }
 
     content.gsub!('zerolog.'){ 'log.' }
