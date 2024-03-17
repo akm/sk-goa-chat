e264168ef3de6741832ebc27f8798798560d3583
akm <akm2000@gmail.com>
2024-03-17 17:57:40 +0900

    :shower: fukowl を削除

--- vendor/fukowl/lib/fukowl/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Fukowl
-  VERSION = "0.1.1"
-end
--- vendor/fukowl/makefiles/uisvr/openapi.mk
+++
@@ -1,6 +0,0 @@
-$(PATH_TO_OPENAPI_TS_FILE):
-	$(MAKE) openapi_gen
-
-.PHONY: openapi_gen
-openapi_gen:
-	npx openapi-typescript $(PATH_TO_OPENAPI_DEFINITION_FILE) -o $(PATH_TO_OPENAPI_TS_FILE)
