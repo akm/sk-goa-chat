74734422e5eecb08e6ba3c49db415c080fb57706
akm <akm2000@gmail.com>
2024-03-17 20:45:41 +0900

    :wrench: :recycle: PATH_TO_GOAVELTE_MAKEFILES の定義を .config.mk に移動

--- .config.mk
+++ .config.mk
@@ -2,6 +2,7 @@ GOOGLE_CLOUD_PROJECT=sk-goa-chat
 APP_MYSQL_DATABASE_NAME?=sk_goa_chat_db
 APP_FIREBASE_API_KEY?=firebase-api-key-dummy1
 
+PATH_TO_GOAVELTE_MAKEFILES=$(PATH_TO_PROJECT)/vendor/goavelte/makefiles
 include $(PATH_TO_GOAVELTE_MAKEFILES)/default/app_stage.mk
 include $(PATH_TO_GOAVELTE_MAKEFILES)/default/ports.mk
 include $(PATH_TO_GOAVELTE_MAKEFILES)/default/directories.mk
