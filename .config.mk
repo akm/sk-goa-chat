8df87092bcc993d8b1ae6358980ce6f115d9ef97
akm <akm2000@gmail.com>
2024-03-17 18:14:24 +0900

    :wrench: ポートの設定を goavelte の default/ports.mk で行うように

--- .config.mk
+++ .config.mk
@@ -49,32 +49,7 @@ endif
 # この環境変数は、Makefie で処理を開始する際に設定します。
 # つまりmakeのターゲットの実行時にのみ決定するので、静的に指定するものではありません。
 
-
-# ## APP_PORT
-#
-# APP_ENV                        | APP_STAGE_TYPE           | apisvr HTTP | apisvr gRPC | ui HTTP  | mysql    | firebase authentication | swagger ui
-# -------------------------------|--------------------------|-------------|-------------|----------|----------|-------------------------|------------
-# server                         | staging,production       | 8000        | 8080        | 4173     | 3306     | ?                       |
-# dev                            | local                    | 8000        | 8080        | 5173     | 3306     | 9099                    | 8090
-# clients/uisvr/test:integration | staging,production       | 8000        | 8080        | 4173     | 3306     | ?                       |
-# clients/uisvr/test:integration | local,github             | 8001        | 8081        | 4173     | 3307     | 9090                    |
-# clients/uisvr/test:unit        | local,github             | -           | -           | -        | -        | -                       |
-# servers/apisvr/test            | local,github             | -           | -           | -        | 3311     | 9091                    |
-
-APP_PORT_APISVR_HTTP_dev?=8000
-APP_PORT_APISVR_HTTP_e2e_test?=8001
-APP_PORT_APISVR_GRPC_dev?=8080
-APP_PORT_APISVR_GRPC_e2e_test?=8081
-APP_PORT_UISVR_HTTP_dev?=5173
-APP_PORT_UISVR_HTTP_e2e_test?=4173
-APP_PORT_MYSQL_dev?=3306
-APP_PORT_MYSQL_e2e_test?=3307
-APP_PORT_MYSQL_unit_test?=3311
-APP_PORT_FIREBASE_AUTH_dev?=9099
-APP_PORT_FIREBASE_AUTH_e2e_test?=9090
-APP_PORT_FIREBASE_AUTH_unit_test?=9091
-APP_PORT_FIREBASE_EMULATOR_SUITE_dev?=4000
-APP_PORT_SWAGGERUI_dev?=8090
+include $(PATH_TO_GOAVELTE_MAKEFILES)/default/ports.mk
 
 ## MySQL の設定
 
