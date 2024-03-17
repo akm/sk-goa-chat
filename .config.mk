d52375b374a8dd1e6f343c87d547432bd04210d9
akm <akm2000@gmail.com>
2024-03-17 18:27:50 +0900

    :wrench: .config.mk の APP_ENV 関連の設定を goavelte に移動

--- .config.mk
+++ .config.mk
@@ -1,19 +1,5 @@
 include $(PATH_TO_GOAVELTE_MAKEFILES)/default/app_stage.mk
 
-## APP_ENV
-#
-# (正確に言えば process cluster type みたいな名前のはずですが分かりやすい名前じゃないので APP_ENV にしました)
-#
-# APP_ENV | 起動するプロセス群
-# ---------|------------------
-# - server | uisvr, apisvr
-# - dev     | uisvr, apisvr, mysql, firebase_auth
-# - e2e_test | uisvr, apisvr, mysql, firebase_auth, clients/uisvr/test/integration
-# - unit_test | uisvr のテスト( clients/uisvr/test/integration 以外), servers 以下のテスト
-#
-# この環境変数は、Makefie で処理を開始する際に設定します。
-# つまりmakeのターゲットの実行時にのみ決定するので、静的に指定するものではありません。
-
 include $(PATH_TO_GOAVELTE_MAKEFILES)/default/ports.mk
 
 ## MySQL の設定
