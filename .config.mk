78cd98472c3419bdedda853cabe5fcbab9190d88
akm <akm2000@gmail.com>
2024-03-17 18:29:30 +0900

    :wrench: :recycle: .config.mk の記述をシンプルに

--- .config.mk
+++ .config.mk
@@ -1,19 +1,7 @@
-include $(PATH_TO_GOAVELTE_MAKEFILES)/default/app_stage.mk
-
-include $(PATH_TO_GOAVELTE_MAKEFILES)/default/ports.mk
-
-## MySQL の設定
-
-APP_MYSQL_DATABASE_NAME?=sk_goa_chat_db
-
-## gcloud の設定
-
 GOOGLE_CLOUD_PROJECT=sk-goa-chat
-
-## Firebase の設定
-
+APP_MYSQL_DATABASE_NAME?=sk_goa_chat_db
 APP_FIREBASE_API_KEY?=firebase-api-key-dummy1
 
-## 各ディレクトリへのパス
-
+include $(PATH_TO_GOAVELTE_MAKEFILES)/default/app_stage.mk
+include $(PATH_TO_GOAVELTE_MAKEFILES)/default/ports.mk
 include $(PATH_TO_GOAVELTE_MAKEFILES)/default/directories.mk
