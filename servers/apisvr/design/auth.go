29d6af7caa50cbcd6437882b1bfb0e354b30bdc7
akm <akm2000@gmail.com>
2024-03-19 08:35:08 +0900

    :shower: 不要な lint の抑止を削除

--- servers/apisvr/design/auth.go
+++ servers/apisvr/design/auth.go
@@ -2,11 +2,9 @@ package design
 
 import "goa.design/goa/v3/dsl"
 
-//nolint:unused
 const authApiKeyScheme = "api_key"
 const authApiKeyName = "id_token"
 
-//nolint:unused
 var authApiKeySecurity = dsl.APIKeySecurity(authApiKeyScheme, func() {
 })
 
