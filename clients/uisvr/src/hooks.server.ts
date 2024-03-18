3ce39be682c30cb18e7ff869e3da8e80bacee061
akm <akm2000@gmail.com>
2024-03-19 08:05:52 +0900

    :sparkles: uisvr を X-ID-TOKEN ではなく X-UID を期待するように変更

--- clients/uisvr/src/hooks.server.ts
+++ clients/uisvr/src/hooks.server.ts
@@ -2,56 +2,15 @@ import type { Handle } from '@sveltejs/kit';
 // import { FirebaseError } from 'firebase-admin';
 
 import { auth } from '$lib/server/firebase-admin';
-import type { DecodedIdToken } from 'firebase-admin/auth';
-import { isFirebaseError } from '$lib/firebase';
-import { isMessageKey } from '$lib/firebase/auth';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const idToken = event.request.headers.get('X-ID-TOKEN');
-	if (!idToken) {
+	const uid = event.request.headers.get('X-UID');
+	if (!uid) {
 		return await resolve(event);
 	}
-	event.locals.idToken = idToken;
 
-	let decodedToken: DecodedIdToken;
-	try {
-		decodedToken = await auth.verifyIdToken(idToken);
-		// decoded の中身の例
-		// {
-		// 	email: 'foo1@example.com',
-		// 	email_verified: false,
-		// 	auth_time: 1704273607,
-		// 	user_id: 'nf5LAN4rZdK3vgNQPikElX1I5Dri',
-		// 	firebase: { identities: { email: [Array] }, sign_in_provider: 'password' },
-		// 	iat: 1704273607,
-		// 	exp: 1704705607,
-		// 	aud: 'sk-goa-chat',
-		// 	iss: 'https://session.firebase.google.com/sk-goa-chat',
-		// 	sub: 'nf5LAN4rZdK3vgNQPikElX1I5Dri',
-		// 	uid: 'nf5LAN4rZdK3vgNQPikElX1I5Dri'
-		// }
-		//
-		// firebase.identities.email は [ 'foo1@example.com' ]
-	} catch (err) {
-		if (isFirebaseError(err)) {
-			if (isMessageKey(err.code)) {
-				switch (err.code) {
-					case 'auth/id-token-expired':
-					case 'auth/id-token-revoked':
-					case 'auth/invalid-id-token':
-					case 'auth/session-cookie-revoked':
-					case 'auth/user-not-found':
-						event.locals.user = undefined;
-						event.locals.idToken = undefined;
-						return await resolve(event);
-				}
-			}
-		}
-		console.error('verifyIdToken error', err);
-		throw err;
-	}
+	const user = await auth.getUser(uid);
 
-	const user = await auth.getUser(decodedToken.uid);
 	event.locals.user = {
 		id: user.uid,
 		name: user.displayName,
